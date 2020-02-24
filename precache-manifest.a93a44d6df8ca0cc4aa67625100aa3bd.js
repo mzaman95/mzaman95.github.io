6cf9f3b717ed955ed7cab29e19c88d8ce612f09c
Mridula Zaman <mridula.zaman95@gmail.com>
2020-02-23 18:22:54 -0600

    build contents

---
+++ precache-manifest.a93a44d6df8ca0cc4aa67625100aa3bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c819e25c7d089c41b2e4a116234d1269",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6cb2d938cda3d21fbfc",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "007f1b2b63c626c27645",
+    "url": "/static/css/main.78164555.chunk.css"
+  },
+  {
+    "revision": "a6cb2d938cda3d21fbfc",
+    "url": "/static/js/2.85777d95.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.85777d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "007f1b2b63c626c27645",
+    "url": "/static/js/main.f198f321.chunk.js"
+  },
+  {
+    "revision": "3d1f0ded6efd343f4c26",
+    "url": "/static/js/runtime-main.d2b1f361.js"
+  }
+]);
