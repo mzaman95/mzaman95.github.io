6345b5c65caad9e5d90014f1d71f1c23e6a458bb
Mridula Zaman <mridula.zaman95@gmail.com>
2020-02-23 18:20:21 -0600

    deleted README.md

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  Navbar,
+  Nav
+} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">{}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto nav-menu-center">
+            <Nav.Link className="nav-link-center" href="#home">Home</Nav.Link>
+            <Nav.Link className="nav-link-center" href="#about">About</Nav.Link>
+            <Nav.Link className="nav-link-center" href="#education">Education</Nav.Link>
+            <Nav.Link className="nav-link-center" href="#experience">Experience</Nav.Link>
+            <Nav.Link className="nav-link-center" href="#projects">Projects</Nav.Link>
+            <Nav.Link className="nav-link-center" href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="App-content">
+        <h1>Hi, my name is Mridula Zaman.</h1>
+        <h2>I'm a front-end web developer.</h2>
+      </div>
     </div>
   );
 }
